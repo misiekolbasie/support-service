ce7e7fd1b31d16aa9377b3a57ef89092241965d7
Anton <doskimag2016@yandex.ru>
2021-03-31 20:05:52 +0300

    add ticket controller, ref json

---
+++ SupportService/Controllers/TicketsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace SupportService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ILogger<TicketsController> _logger;
+
+        public TicketsController(ILogger<TicketsController> logger)
+        {
+            _logger = logger ?? new NullLogger<TicketsController>();
+        }
+    }
+}
