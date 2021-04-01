01feb7b648bd9b6b729672ba0e0e0de532398f83
Anton <doskimag2016@yandex.ru>
2021-04-01 11:24:25 +0300

    user controller, входные параметры для метода create user

---
+++ SupportService/ApiDto/CreateUserRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SupportService.Models.Enums;
+
+namespace SupportService.ApiDto
+{
+    public class CreateUserRequest
+    {
+        public string Name { get; set; }
+
+        public Roles Roles { get; set; }
+    }
+}
