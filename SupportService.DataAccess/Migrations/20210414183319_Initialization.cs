732977dfbc06a3a81c7867e28b83dfc077692846
Anton <doskimag2016@yandex.ru>
2021-04-14 21:38:10 +0300

    добавил миграции

---
+++ SupportService.DataAccess/Migrations/20210414183319_Initialization.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace SupportService.DataAccess.Migrations
+{
+    public partial class Initialization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    role = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tickets",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    theme = table.Column<string>(type: "text", nullable: true),
+                    autorId = table.Column<int>(type: "integer", nullable: false),
+                    createData = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    lastUpdate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    category = table.Column<int>(type: "integer", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tickets", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_tickets_users_autorId",
+                        column: x => x.autorId,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "messages",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ticketId = table.Column<int>(type: "integer", nullable: false),
+                    text = table.Column<string>(type: "text", nullable: true),
+                    createDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    autorId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_messages", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_messages_tickets_ticketId",
+                        column: x => x.ticketId,
+                        principalTable: "tickets",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_messages_users_autorId",
+                        column: x => x.autorId,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_autorId",
+                table: "messages",
+                column: "autorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_ticketId",
+                table: "messages",
+                column: "ticketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tickets_autorId",
+                table: "tickets",
+                column: "autorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "messages");
+
+            migrationBuilder.DropTable(
+                name: "tickets");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
