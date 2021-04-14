2f9b3542ed38819c83af7ab4d0ca58619644aec1
Anton <doskimag2016@yandex.ru>
2021-04-14 22:49:29 +0300

    db context in ticket repo, register like transient

--- SupportService.DataAccess/Repositories/TicketRepository.cs
+++ SupportService.DataAccess/Repositories/TicketRepository.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using SupportService.DataAccess.Entities;
 using SupportService.DataAccess.Repositories.Interfaces;
 using SupportService.Models.Enums;
 using SupportService.Models.Models;
@@ -14,74 +15,122 @@ namespace SupportService.DataAccess.Repositories
     public class TicketRepository : ITicketRepository
     {
         private readonly ILogger<TicketRepository> _logger;
-        private readonly List<Ticket>_ticketsDb = new List<Ticket>();
+        private readonly SupportServiceDbContext _ticketsDb;
 
-        public TicketRepository(ILogger<TicketRepository> logger)
+        public TicketRepository(SupportServiceDbContext dbContext, ILogger<TicketRepository> logger)
         {
+            _ticketsDb = dbContext;
             _logger = logger ?? new NullLogger<TicketRepository>();
         }
         public int CreateTicket(Ticket ticket)
         {
-            int maxCount = _ticketsDb.Count;
-            ticket.Id = maxCount;
-            _ticketsDb.Add(ticket);
-            return maxCount;
+            //ticket v ticket entity , pishem property
+            TicketEntity ticketEntity = new TicketEntity()
+            {
+                AutorId = ticket.AutorId,
+                Category = (int)ticket.Category, //upcast
+                CreateDate = ticket.CreateDate,
+                LastUpdate = ticket.LastUpdate,
+                Status = (int)ticket.Status,
+                Theme = ticket.Theme
+            };
+            // save in base
+            _ticketsDb.Tickets.Add(ticketEntity); //base.Tablica.add(object entity(stroka v bd))
+            _ticketsDb.SaveChanges();// save base
+            //return id
+            return ticketEntity.Id;
+
         }
 
         public IEnumerable<Ticket> GetAllTickets()
         {
-            var tickets = _ticketsDb.ToList();
+            // go v bazy, vzyat vse etity ticketov
+            List<TicketEntity> ticketEntities = _ticketsDb.Tickets.ToList();
+            // entity prevratit v modely
+            List<Ticket> tickets = new List<Ticket>();
+            foreach (var entity in ticketEntities)
+            {
+                Ticket ticket = TicketEntityToModel(entity);
+                tickets.Add(ticket);
+            }
+            // vernyt modely
             return tickets;
         }
 
+
+
         public IEnumerable<Ticket> GetTicketsByUserId(int userId)
         {
-            // ������� ������ ����
+            // zaprosit vse entity 
+            List<TicketEntity> ticketEntities = _ticketsDb.Tickets.ToList();
             List<Ticket> userTickets = new List<Ticket>();
-            // ���������� � ��������� �������, ����� ����� ��
-            foreach (var ticket in _ticketsDb)
+            foreach (var entity in ticketEntities)
             {
-                // ���� �� ����� ����� �� ������ �������� �� � ������ ����
+                Ticket ticket = TicketEntityToModel(entity);
+                // iz entity v ticket
                 if (ticket.AutorId == userId)
                 {
                     userTickets.Add(ticket);
                 }
             }
             return userTickets;
-            //������� ����
         }
 
         public void ChangeStatus(int ticketId, Statuses newStatus)
         {
-            // ���������� � ��������� �������, � ����� ����� �� ��
-            foreach (var ticket in _ticketsDb)
+            // find ticket entity po id
+            TicketEntity entity = _ticketsDb.Tickets.FirstOrDefault(entity => entity.Id == ticketId); //entity - SYSCHESTVYET TOLKO V ETIX SKOBKAX => entity.Id == ticketId
+            //proverka est' li entity voobshe
+            if (entity == null)
             {
-                if (ticket.Id == ticketId)
-                {
-                    //�������� ������ � ������ �� �����
-                    ticket.Status = newStatus;
-                    break;
-                }
+                throw new Exception("Ticket not found");
             }
-            throw new Exception("����� �� ������!");
+            // pomenyat status
+            entity.Status = (int)newStatus;
+            //save BASE
+            _ticketsDb.SaveChanges();
         }
 
         public Ticket GetTicketById(int ticketId)
         {
-            foreach (var ticket in _ticketsDb)
+            // find ticket entity po id
+            TicketEntity entity = _ticketsDb.Tickets.FirstOrDefault(entity => entity.Id == ticketId); //entity - SYSCHESTVYET TOLKO V ETIX SKOBKAX => entity.Id == ticketId
+            //proverka est' li entity voobshe
+            if (entity == null)
             {
-                if (ticket.Id == ticketId)
-                {
-                    return ticket;
-                }
+                return null;
             }
-            return null;
+            // preobrazovanie iz entity v model
+            Ticket ticket = TicketEntityToModel(entity);
+            return ticket;
         }
 
-        public void ChangeLastUpdate(in int ticketId, in DateTime lastUpdate)
+        public void ChangeLastUpdate(int ticketId, DateTime lastUpdate)
         {
-            Ticket ticket = GetTicketById(ticketId);
-            ticket.LastUpdate = lastUpdate;
+            // find ticket entity po id
+            TicketEntity entity = _ticketsDb.Tickets.FirstOrDefault(entity => entity.Id == ticketId); //entity - SYSCHESTVYET TOLKO V ETIX SKOBKAX => entity.Id == ticketId
+            //proverka est' li entity voobshe
+            if (entity == null)
+            {
+                throw new Exception("Ticket not found");
+            }
+            // novoe vremya i save in base
+            entity.LastUpdate = lastUpdate;
+            _ticketsDb.SaveChanges();
+        }
+
+        private Ticket TicketEntityToModel(TicketEntity entity)
+        {
+            Ticket ticket = new Ticket()
+            {
+                AutorId = entity.AutorId,
+                Category = (Categories)entity.Category,
+                CreateDate = entity.CreateDate,
+                LastUpdate = entity.LastUpdate,
+                Status = (Statuses)entity.Status,
+                Theme = entity.Theme
+            };
+            return ticket;
         }
     }
 }
--- SupportService/Startup.cs
+++ SupportService/Startup.cs
@@ -37,7 +37,7 @@ namespace SupportService
             services.AddTransient<ITicketService, TicketService>();
             services.AddTransient<IMessageService, MessageService>();
             services.AddSingleton<IUserRepository, UserRepository>();
-            services.AddSingleton<ITicketRepository, TicketRepository>();
+            services.AddTransient<ITicketRepository, TicketRepository>();
             services.AddSingleton<IMessageRepository, MessageRepository>();
             services.AddDbContext<SupportServiceDbContext>(options =>
                 options.UseNpgsql(@"Server = localhost; Port = 5432; Database = support_service_db; User Id = postgres; Password = 1qaz@WSX"));
